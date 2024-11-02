65fb38ab93ff3bff7ae1df088fb2969c7aac69f5
krutheesh <krutheeshsiripuram@gmail.com>
2024-11-02 12:39:01 +0530

    logout

--- controller/user/userLogout.js
+++ controller/user/userLogout.js
@@ -1,29 +1,24 @@
-async function userLogout(req,res){
-    try{
-        res.clearCookie("token", { 
+async function userLogout(req, res) {
+  try {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      sameSite: "none",
+      secure: true,
+    });
 
-            secure: true, 
-    
-            sameSite: 'none', 
-    
-            httpOnly: true 
-    
-        })
-
-        res.json({
-            message : "Logged out successfully",
-            error : false,
-            success : true,
-            data : []
-        })
-    }catch(err){
-        res.json({
-            message : err.message || err  ,
-            error : true,
-            success : false,
-        })
-    }
+    res.json({
+      message: "Logged out successfully",
+      error: false,
+      success: true,
+      
+    });
+  } catch (err) {
+    res.json({
+      message: err.message || err,
+      error: true,
+      success: false,
+    });
+  }
 }
 
-
-module.exports = userLogout
+module.exports = userLogout;
